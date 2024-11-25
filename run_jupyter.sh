9f7d686df0ba65912c2327be6219957d8139e193
lucianoSF <lukfraga@hotmail.com>
2024-11-25 15:47:33 +0000

    fix sleep command

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -2,6 +2,6 @@ docker build -t jupyter/jupyter-free5gc .
 
 docker run -d --name jupyterlab --rm -it -p 8888:8888   -w "/home/jovyan"   -v "$(pwd):/home/jovyan/mount/" jupyter/jupyter-free5gc
 
-sleep(3)
+sleep 3
 
 docker logs jupyterlab
